ccc01498e7c3dc993fe61efd4b9a9447b8c4a4de
ckury <155270699+ckury@users.noreply.github.com>
2024-03-01 16:34:29 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request
+from google.cloud import datastore
+
+app = Flask(__name__)
+
+# Initialize the Datastore client
+client = datastore.Client()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit_company_info', methods=['POST'])
+def submit_company_info():
+    if request.method == 'POST':
+        yahoo_ticker = request.form['yahoo_ticker']
+        industry = request.form['industry']
+        long_name = request.form['long_name']
+
+        # Check if the Yahoo ticker already exists in the database
+        query = client.query(kind='CompanyInfo')
+        query.add_filter('YahooTicker', '=', yahoo_ticker)
+        results = list(query.fetch(limit=1))
+
+        # If the ticker exists, update the entity
+        if results:
+            company_entity = results[0]  # Get the existing entity
+            company_entity.update({
+                'YahooTicker': yahoo_ticker,
+                'Industry': industry,
+                'LongName': long_name,
+            })
+            client.put(company_entity)
+            return 'Yahoo ticker information updated successfully!'
+        else:
+            # If the ticker doesn't exist, create a new entity
+            key = client.key('CompanyInfo')
+            company_entity = datastore.Entity(key=key)
+            company_entity.update({
+                'YahooTicker': yahoo_ticker,
+                'Industry': industry,
+                'LongName': long_name,
+            })
+            client.put(company_entity)
+            return 'Company information submitted successfully!'
+
+    # If the request method is not POST, just return the form page
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
