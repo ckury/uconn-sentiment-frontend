4cce4aafbba1af592756e386fa1a5fd32200e285
ckury <155270699+ckury@users.noreply.github.com>
2024-04-04 14:54:17 -0400

    Code Cleanup

--- dataplots/graphing.py
+++ dataplots/graphing.py
@@ -3,41 +3,22 @@ import plotly.graph_objs as go
 from plotly.offline import plot
 
 def data_plot(kind='Banks', ticker=None, sector=None, weighted="False", graphtype="icat", startmonth=None, endmonth=None):
-    # Initialize the Datastore client
+    # Initialize Datastore Client
     client = datastore.Client()
 
-    # Cleaning up the tickers and putting them into a list
-    clean_tickers = []
+    # Converting string provided by URL to boolean value
+    if weighted == "True": weighted = True; scoreColumn = "WeightedSentiment"
+    if weighted == "False": weighted = False; scoreColumn = "Score"
 
-    if "," in ticker:
-        ticker = ticker.split(",")
-        for t in ticker:
-            clean_tickers.append(t.strip().upper())
+    # Cleanup tickers and sectors and put them in correct format
+    clean_tickers = input_cleanup(ticker)
+    clean_sectors = input_cleanup(sector)
 
-    else:
-        clean_tickers.append(ticker.strip().upper())
-
-    # Cleaning up the sectors and putting them into a list
-    clean_sectors = []
-
-    if "," in sector:
-        sector = sector.split(",")
-        for s in sector:
-            clean_tickers.append(s.strip().upper())
-
-    else:
-        clean_sectors.append(sector.strip().upper())
-
-
-
-    # Create a query to fetch entities from the Datastore
-    query = client.query(kind=kind)
-    query.order = ['YahooTicker']
-
-    # Fetch the data
+    # Try to fetch the data
     try:
-        entities = list(query.fetch())
-    
+        entities = fetch_data(client=client, kind=kind)
+
+    # If data fetch fails, return error code
     except:
         return 429
 
@@ -50,27 +31,10 @@ def data_plot(kind='Banks', ticker=None, sector=None, weighted="False", graphtyp
     else:
         filtered_entities = [e for e in entities if e['YahooTicker'] in clean_tickers]
 
-    if weighted == "True":
-        scoreColumn = "WeightedSentiment"
-        weightTitle = "Weighted"
-
-    elif weighted == "False":
-        scoreColumn = "Score"
-        weightTitle = "Unweighted"
-
     for entity in filtered_entities:
-        period = entity['Period']
+        period = prepare_period(entity['Period'])
         category = entity['YahooTicker'] + ": " + entity['Category']
         score = entity[scoreColumn]
-
-        if period[0] == "Q":
-            quarter = period[1]
-            period = period[2:]
-
-            period = period + "Q" + quarter
-
-        else:
-            period = period
 
         if category not in keyword_period_scores:
             keyword_period_scores[category] = {}
@@ -79,22 +43,6 @@ def data_plot(kind='Banks', ticker=None, sector=None, weighted="False", graphtyp
             keyword_period_scores[category][period].append(score)
         else:
             keyword_period_scores[category][period] = [score]
-
-
-
-    titleTicker, titleSector = "", ""
-
-    for ticker in clean_tickers:
-        titleTicker = titleTicker + ticker + ", "
-
-    titleTicker = titleTicker[:-2]
-
-    for sector in clean_sectors:
-        titleSector = titleSector + sector + ", "
-
-    titleSector = titleSector[:-2]
-
-
 
     # Prepare the Plotly graph
     fig = go.Figure()
@@ -113,13 +61,79 @@ def data_plot(kind='Banks', ticker=None, sector=None, weighted="False", graphtyp
         ))
 
     fig.update_layout(
-        title=f'{weightTitle} Sentiment Scores Over Time for Tickers: ({titleTicker}) and Sectors: ({titleSector})',
+        title=title_creation(clean_tickers, clean_sectors, weighted),
         xaxis_title='Period',
-        yaxis_title='Average Sentiment Score',
-        legend_title='Categories',
+        yaxis_title='Sentiment Score',
+        legend_title='Legend',
         xaxis=dict(tickangle=45),
         yaxis_range=[-1, 1]
     )
 
     # Return HTML div
     return plot(fig, include_plotlyjs=True, output_type='div')
+
+def input_cleanup(input):
+    # Cleaning up the tickers and putting them into a list
+    output = []
+
+    # Multiple tickers separated and cleaned up
+    if "," in input:
+        input = input.split(",")
+        for i in input:
+            output.append(i.strip().upper())
+
+    # Single ticker cleaned up
+    else:
+        output.append(input.strip().upper())
+    
+    return output
+
+def title_creation(tickers, sectors, weighted):
+    output = ""
+
+    if weighted is True: output += 'Weighted '
+    if weighted is False: output += 'Unweighted '
+    
+    output += "Sentiment Scores Over Time Including "
+
+    if '' not in tickers:
+        output += "Tickers: "
+        for t in tickers:
+            output += t
+            output += ', '
+        
+        output = output[:-2]
+        if '' not in sectors:
+            output += " and "
+
+    if '' not in sectors:
+        output += "Sectors: "
+        for s in sectors:
+            output += s
+            output += ', '
+        output = output[:-2]
+
+    return output
+
+def fetch_data(client, kind):
+    # Create a query to fetch entities from the Datastore
+    query = client.query(kind=kind)
+    query.order = ['YahooTicker']
+    
+    # Return a list of the entities
+    return list(query.fetch())
+
+def prepare_period(input):
+    if input[0] == "Q":
+        quarter = input[1]
+        output = input[2:]
+
+        output += "Q" + quarter
+
+    else:
+        output = input
+
+    return output
+
+def tickers_from_sectors(client, sector):
+    return
