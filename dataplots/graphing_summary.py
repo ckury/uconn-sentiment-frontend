32ebf27bbabf42dd18480f13c96a61be2f5e1a04
ckury <155270699+ckury@users.noreply.github.com>
2024-04-07 01:47:45 -0400

    Added comments, docstrings, filetypes. Cleaned up code

--- dataplots/graphing_summary.py
+++ dataplots/graphing_summary.py
@@ -1,17 +1,32 @@
+import os, sys
+root_path = os.path.abspath('..')
+sys.path.append(root_path)
+
 from google.cloud import datastore
 import plotly.graph_objs as go
 from plotly.offline import plot
+from utils.tickers_from_sectors import tickers_from_sectors
 
-def data_plot_summary(ticker=None, sector=None, weighted="False", startmonth=None, endmonth=None):
-    # Prepare the Plotly graph
+def data_plot_summary(ticker: str | list=None,
+                      sector: str | list=None, 
+                      weighted: str | bool=False, 
+                      startmonth=None, 
+                      endmonth=None) -> str:
+    '''
+    This function takes in tickers and/or sectors with filtering options and returns a graph HTML div in the form of a str \n
+    Note: This graph pulls from summary data kind in GCP and displays each ticker as a separate trace on graph. The sectors 
+    are flattened into individual tickers and are displayed as if they were inputted individually
+    '''
+
+    # Initialize plotly graph instance
     fig = go.Figure()
     
     # Initialize Datastore Client
     client = datastore.Client()
 
     # Converting string provided by URL to boolean value
-    if weighted == "True": weighted = True; scoreColumn = "Weighted Average"
-    if weighted == "False": weighted = False; scoreColumn = "Total Average"
+    if weighted in [True, "True"]: weighted = True; scoreColumn = "Weighted Average"
+    if weighted in [False, "False"]: weighted = False; scoreColumn = "Total Average"
 
     # Cleanup tickers and sectors and put them in correct format
     clean_tickers = input_cleanup(ticker)
@@ -19,17 +34,19 @@ def data_plot_summary(ticker=None, sector=None, weighted="False", startmonth=Non
 
     # Try to fetch the data
     try:
-        entities = fetch_data(client=client, kind="Banks_Summary")
+        entities = fetch_ticker_data(client=client, kind="Banks_Summary")
 
-    # If data fetch fails, return error code
+    # If data fetch fails, return catch error and provide error code as response
     except:
         return 429
 
     # Filter entities after fetching
-    if "BANKS" in clean_sectors:
+    if "BANKS" in clean_sectors: # TODO: Currently this is a temporary workaround, This should be switched to querying for tickers in the sector using the tickers_from_sectors function imported
         filtered_entities = entities
     else:
         filtered_entities = [e for e in entities if e['Yahoo Ticker'] in clean_tickers]
+
+    # Query entity disection and data arrangement preparation for adding trace to graph. This takes rows and consolidating them into a dictionary with the tickers as kinds.
 
     scoreDictionary = {}
 
@@ -44,7 +61,7 @@ def data_plot_summary(ticker=None, sector=None, weighted="False", startmonth=Non
             scoreDictionary[tickerTrace] = [(period, score)]
 
 
-    
+    # Take data out of dictionary and plot trace
 
     for tickerTrace, data in scoreDictionary.items():
         periods = []
@@ -62,6 +79,8 @@ def data_plot_summary(ticker=None, sector=None, weighted="False", startmonth=Non
             mode='lines+markers',
             name=tickerTrace
         ))
+
+    # Add layout configurations to the graph
         
     fig.update_layout(
         title=title_creation(clean_tickers, clean_sectors, weighted),
@@ -72,11 +91,18 @@ def data_plot_summary(ticker=None, sector=None, weighted="False", startmonth=Non
         yaxis_range=[-1, 1]
     )
 
-    # Return HTML div
+    # Return HTML div to main flask app
     return plot(fig, include_plotlyjs=True, output_type='div')
 
-def input_cleanup(input):
-    # Cleaning up the tickers and putting them into a list
+def input_cleanup(input: str | list) -> list:
+    '''
+    This function takes an input string gathered from a URL and removes unnessesary spaces, separates tickers by comma and returns them as list.\n
+    If list is provided, processing is ignored and original list is returned
+    '''
+
+    if input is list:
+        return input
+
     output = []
 
     # Multiple tickers separated and cleaned up
@@ -91,7 +117,7 @@ def input_cleanup(input):
     
     return output
 
-def title_creation(tickers, sectors, weighted):
+def title_creation(tickers: list, sectors: list, weighted: bool) -> str: 
     output = ""
 
     if weighted is True: output += 'Weighted '
@@ -118,7 +144,7 @@ def title_creation(tickers, sectors, weighted):
 
     return output
 
-def fetch_data(client, kind):
+def fetch_ticker_data(client: datastore.Client, kind: str) -> list:
     # Create a query to fetch entities from the Datastore
     query = client.query(kind=kind)
     query.order = ['Yahoo Ticker']
@@ -126,7 +152,10 @@ def fetch_data(client, kind):
     # Return a list of the entities
     return list(query.fetch())
 
-def prepare_period(input):
+def prepare_period(input: str) -> str:
+    '''
+    This function outputs period in the form of YYYYQQ whether input is QQYYYY or YYYYQQ
+    '''
     if input[0] == "Q":
         quarter = input[1]
         output = input[2:]
@@ -137,6 +166,3 @@ def prepare_period(input):
         output = input
 
     return output
-
-def tickers_from_sectors(client, sector):
-    return
