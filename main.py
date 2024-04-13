0a74e41aa8b7fcc9e7900df614629dfc6be915b3
ckury <155270699+ckury@users.noreply.github.com>
2024-04-12 20:23:53 -0400

    Removed old company info form to prepare for new table

--- main.py
+++ main.py
@@ -84,43 +84,6 @@ def table():
 
     return output
 
-@app.route('/submit_company_info', methods=['POST'])
-def submit_company_info():
-    if request.method == 'POST':
-        yahoo_ticker = request.form['yahoo_ticker']
-        industry = request.form['industry']
-        long_name = request.form['long_name']
-
-        # Check if the Yahoo ticker already exists in the database
-        query = datastoreClient.query(kind='CompanyInfo')
-        query.add_filter('YahooTicker', '=', yahoo_ticker)
-        results = list(query.fetch(limit=1))
-
-        # If the ticker exists, update the entity
-        if results:
-            company_entity = results[0]  # Get the existing entity
-            company_entity.update({
-                'YahooTicker': yahoo_ticker,
-                'Industry': industry,
-                'LongName': long_name,
-            })
-            datastoreClient.put(company_entity)
-            return 'Yahoo ticker information updated successfully!'
-        else:
-            # If the ticker doesn't exist, create a new entity
-            key = datastoreClient.key('CompanyInfo')
-            company_entity = datastore.Entity(key=key)
-            company_entity.update({
-                'YahooTicker': yahoo_ticker,
-                'Industry': industry,
-                'LongName': long_name,
-            })
-            datastoreClient.put(company_entity)
-            return 'Company information submitted successfully!'
-
-    # If the request method is not POST, just return the form page
-    return render_template('index.html')
-
 @app.route('/upload_file', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
